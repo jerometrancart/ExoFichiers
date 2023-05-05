692f023eabfa8c9746b6f42f38ef248abe7cddda
jerometrancart <jerome.trancart@gmail.com>
2023-05-05 04:08:30 +0200

    File manipulation practiced

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+const string path = @"C:\wamp64\www\.Net\Section_10\Exofichiers\data";
+
+string? saisie = " ";
+while (string.IsNullOrWhiteSpace(saisie))
+{
+    System.Console.WriteLine("Veuillez saisir votre nom et votre prénom");
+    saisie = Console.ReadLine();
+}
+
+if(!Directory.Exists(path)){
+    Directory.CreateDirectory(path);
+}
+
+if(!File.Exists($"{path}\\identite.txt"))
+{
+    File.Create($"{path}\\identite.txt");
+}    
+
+var text = File.ReadAllText($"{path}\\identite.txt");
+// var bytes = File.ReadAllBytes(@"C:\wamp64\www\.Net\Section_10\Exofichiers\data\identite.bin");
+
+if(!saisie.Equals(text, StringComparison.OrdinalIgnoreCase))
+    {
+        File.WriteAllText($"{path}\\identite.txt", saisie); 
+    }
+
+
+
+
+
