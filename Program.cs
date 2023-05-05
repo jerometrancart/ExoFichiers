d61914bf9262a97117b5ff1066863966a1c52fc9
jerometrancart <jerome.trancart@gmail.com>
2023-05-05 04:33:01 +0200

    added async methods

--- Program.cs
+++ Program.cs
@@ -17,12 +17,12 @@ if(!File.Exists($"{path}\\identite.txt"))
     File.Create($"{path}\\identite.txt");
 }    
 
-var text = File.ReadAllText($"{path}\\identite.txt");
+var text = await File.ReadAllTextAsync($"{path}\\identite.txt");
 // var bytes = File.ReadAllBytes(@"C:\wamp64\www\.Net\Section_10\Exofichiers\data\identite.bin");
 
 if(!saisie.Equals(text, StringComparison.OrdinalIgnoreCase))
     {
-        File.WriteAllText($"{path}\\identite.txt", saisie); 
+        await File.WriteAllTextAsync($"{path}\\identite.txt", saisie); 
     }
 
 
